b0457f54d9700eba24b5df7df2d36ac8e1b11357
KUSH MANIHAR <kush.22jics158@jietjodhpur.ac.in>
2026-04-19 14:00:30 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from telethon import TelegramClient, events
+
+api_id = 31891006  # <-- apna api_id daal
+api_hash = '79068ae528aa4242c45006cb68c89a07' # <-- apna api_hash daal
+
+client = TelegramClient('session', api_id, api_hash)
+
+source_group = '@theprofessionals'
+target_group = '@kushjobs123'
+
+message_map = {}
+
+@client.on(events.NewMessage(chats=source_group))
+async def new_message_handler(event):
+    try:
+        reply_to = None
+
+        if event.is_reply:
+            reply_msg = await event.get_reply_message()
+            reply_to = message_map.get(reply_msg.id)
+
+        sent = await event.message.copy_to(target_group, reply_to=reply_to)
+        message_map[event.message.id] = sent.id
+
+    except Exception as e:
+        print("Error:", e)
+
+
+@client.on(events.MessageEdited(chats=source_group))
+async def edit_handler(event):
+    try:
+        if event.message.id in message_map:
+            await client.edit_message(
+                target_group,
+                message_map[event.message.id],
+                event.message.text or ""
+            )
+    except Exception as e:
+        print("Edit error:", e)
+
+
+client.start()
+print("🔥 Bot chal raha hai...")
+client.run_until_disconnected()
